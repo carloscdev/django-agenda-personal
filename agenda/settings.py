0a750fc706476eef9dc834565281bb04c26ec4b8
Carlos Córdova <carlos_cdo@outlook.com>
2022-09-23 00:14:37 -0500

    🚧 config .env

--- agenda/settings.py
+++ agenda/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kj)qx=8=bhgk*(t%@fp!^0=hi!@efi-@cv1)ar+u0wz-gm^0q('
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,11 +82,11 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'agendadb',
-        'USER': 'carloscdev',
-        'PASSWORD': 'ylZDPvkAh51kWaImiTaCTxgAflqnVEjo',
-        'HOST': 'dpg-cclql782i3mnmmii1hvg-a.oregon-postgres.render.com',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
